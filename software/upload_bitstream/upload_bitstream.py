bd72f65a52ef44bc599a50a46c1ffc3dc3392bd6
IAmMarcelJung <MarcelJung97@web.de>
2024-07-18 14:15:23 +0200

    feat: Check if multiple serial devices are connected and make it possible to specify a device type

--- software/upload_bitstream/upload_bitstream.py
+++ software/upload_bitstream/upload_bitstream.py
@@ -5,6 +5,7 @@ import serial.tools.list_ports
 import argparse
 from termcolor import colored
 from pathlib import Path
+from typing import List
 
 
 def parse_arguments() -> argparse.Namespace:
@@ -25,49 +26,59 @@ def parse_arguments() -> argparse.Namespace:
         type=int,
         default=57600,
     )
-    parser.add_argument(
-        "-d",
-        "--device_name",
-        help="Specifies the device name.",
-        required=True,
-        type=str,
-    )
     args = parser.parse_args()
     return args
 
 
-def get_device_port(device_name: str) -> str:
-    """Get the device port for the specified device name.
+def get_device_port(device_name: str = "") -> str:
+    """Get the device port of a connected device.
 
-    :param device_name: The name of the device to get the port for.
-    :type device_name: str
+    :param device_name: The name of the device to be used, default to ""
     :return: The device port for the specified device name.
     :rtype: str
     """
 
     device_port = ""
+    device_names = []
     found_device = False
     ports = serial.tools.list_ports.comports()
 
     # Assign the device port if a matching device was found
     for port in ports:
-        if device_name in str(port.product):
+        if device_name != "":
+            if device_name in str(port.product):
+                device_port = port.device
+            if found_device:
+                print(
+                    colored("Error: ", "red")
+                    + f"""Multiple serial devices of type {device_name} found.
+       Please connect only one device of this type."""
+                )
+                exit(1)
+            found_device = True
+        else:
             device_port = port.device
-        if found_device:
-            print(
-                colored("Error: ", "red")
-                + f"""Multiple devices of type {device_name} found.
-        Please connect only one device."""
-            )
-            exit(1)
-        found_device = True
-        break
+            device_names.append(port.interface)
+            if found_device:
+                print(
+                    colored("Error: ", "red")
+                    + f"""Multiple serial devices found.
+       Please either specify the type or connect only one device."""
+                )
+                _print_device_names(device_names)
+                print(
+                    """
+       It is sufficient to specify the first part of the name:
+       E.g. for \"FT232R USB UART\" just specify \"FT232R\"."""
+                )
+                exit(1)
+            found_device = True
 
     if not found_device:
         print(
             colored("Error: ", "red")
-            + f"""No device of type {device_name} found.
-        Please check the connected devices and the device type"""
+            + f"""No serial device found.
+       Please check if a device is connected"""
         )
         exit(1)
 
@@ -76,6 +87,7 @@ def get_device_port(device_name: str) -> str:
 
 def read_bitstream_data(bitstream_file: str) -> bytearray:
     """Read the bitstream data from the specified file.
+
     :param bitstream_file: The bitstream file to be read.
     :type bitstream_file: str
     :return: The bitstream data read from the file.
@@ -96,13 +108,27 @@ def read_bitstream_data(bitstream_file: str) -> bytearray:
     return data
 
 
+def _print_device_names(device_names: List) -> None:
+    """
+    :param device_names: The device names to be printed.
+    :type device_names: List
+    """
+    print("\n       Found the following devices:")
+    for device_name in device_names:
+        if device_name == None:
+            device_name = "<No name specified>"
+        print(f"       {device_name}")
+
+
 def main() -> None:
     args = parse_arguments()
 
-    device_port = get_device_port(args.device_name)
+    print("Checking device...")
+
+    device_port = get_device_port()
 
     data = read_bitstream_data(args.bitstream_file)
-    print("Uploading bitstream")
+    print("Uploading bitstream...")
 
     with serial.Serial(device_port, args.baudrate) as ser:
         ser.write(data)

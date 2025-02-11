fc51dcc1244867f32389a3488f4e9b05b604d01d
IAmMarcelJung <MarcelJung97@web.de>
2025-02-11 11:48:30 +0100

    doc: improve help message of the baudrate parameter

--- software/board.py
+++ software/board.py
@@ -95,7 +95,8 @@ def setup_parser() -> argparse.Namespace:
     upload_parser.add_argument(
         "-b",
         "--baudrate",
-        help="Specifies the baudrate. Defaults to 57600 which is the eFPGAs baud rate at 10 MHz.",
+        help="Specifies the baudrate. Defaults to 57600 which is the baud rate"
+        + " of MPW-2 at 10 MHz and of MPW-5 at 12.5 MHz.",
         type=int,
         default=57600,
     )
